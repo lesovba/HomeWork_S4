f13e8e587837df6fcab6a9f549b7b9d9f2d2eb5a
Artem Golubev <lesovba@gmail.com>
2023-05-19 17:52:13 +0500

    Completed Task #3

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] FillArray ()
+{
+    Console.WriteLine("Введите длину массива");
+    int arrLength = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Введите {arrLength} элементов массива");
+    int[] array = new int[8];    
+    for (int i = 0; i<8; i++) 
+    {
+    Console.WriteLine($"Введите {i} элемент массива:");
+    array[i] = int.Parse(Console.ReadLine());
+    }
+    return(array); 
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i<arr.Length-1; i++){
+    Console.Write($" {arr[i]}, ");} 
+    // Убираем запятую после последнего элемента в консоли
+    Console.Write($" {arr[7]} ");
+    Console.WriteLine(); 
+}
+
+PrintArray(FillArray());
