0ad62af8b8aadc97156dc5c3ab94eb18fdd41ae8
Artem Golubev <lesovba@gmail.com>
2023-05-19 16:55:04 +0500

    Completed Task #2

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt()
+{
+    Console.WriteLine("Введите число : ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void GetDigitSum (int number)
+{
+    int sum = 0; 
+
+    while (number != 0)
+    {
+        sum = sum + (number%10); 
+        number =  number/10; 
+    }
+    Console.WriteLine(sum);
+}
+
+GetDigitSum(Prompt()); 
